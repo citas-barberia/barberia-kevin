2f236378059d14d37f5fa7c4a1d591aa9cc73d28
Ethan Castillo Corea <ethancastillo0411@gmail.com>
2026-03-21 16:47:49 -0600

    cambio de app.py

--- app.py
+++ app.py
@@ -417,6 +417,7 @@ def citas_json():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
     app.run(host="0.0.0.0", port=port)
 
 
